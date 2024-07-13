85f1ed85cf2b807089bc4736106deb714c622eb1
David Pereira <d_pereira@outlook.com.br>
2024-07-13 18:59:33 -0300

    creating settinfs for fleet direction

--- alien_invasion.py
+++ alien_invasion.py
@@ -76,15 +76,15 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
+
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
-
-    def _update_aliens(self):
-        """Update the positions of all aliens in the fleet."""
-        self.aliens.update()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
