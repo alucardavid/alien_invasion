1e61d94459d865f2f0922fda001bf91c0b116cbb
David Pereira <d_pereira@outlook.com.br>
2024-07-18 22:15:41 -0300

    ending game

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,6 @@
 import pygame.font
-
+from pygame.sprite import Group
+from ship import Ship
 class Scoreboard:
     """A class to report scoring information."""
 
@@ -9,6 +10,7 @@ class Scoreboard:
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
         self.stats = ai_game.stats
+        self.ai_game = ai_game
 
         # Fton settings for scoring information.
         self.text_color = (30, 30, 30)
@@ -18,6 +20,7 @@ class Scoreboard:
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_ships()
 
     def prep_score(self):
         """Turn the score into a redered image."""
@@ -35,6 +38,7 @@ class Scoreboard:
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        self.ships.draw(self.screen)
 
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
@@ -62,3 +66,12 @@ class Scoreboard:
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
+
+    def prep_ships(self):
+        """Show how many ships are left."""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            #self.ships.add(ship)
