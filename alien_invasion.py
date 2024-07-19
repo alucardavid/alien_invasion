4a48319b75b77b581dcaf9a0da6c5f17a504d6a1
David Pereira <d_pereira@outlook.com.br>
2024-07-18 21:34:28 -0300

    updating the score as aliens are shot down

--- alien_invasion.py
+++ alien_invasion.py
@@ -128,6 +128,10 @@ class AlienInvasion:
         #   if so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
+        if collisions:
+            self.stats.score += self.settings.alien_points
+            self.sb.prep_score()
+
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
